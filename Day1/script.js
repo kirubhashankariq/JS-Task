b27e16c1c0787d83b98b007834451009b3cf493d
kirubhashankariq <kirubhashankar.iq@outlook.com>
2026-06-05 18:24:03 +0530

    Task 1 completed

---
+++ Day1/script.js
@@ -0,0 +1,79 @@
+// Task 1
+var studentName = "Naveen";
+const studentAge = 18;
+let studentCourse = "MERN stack"
+
+console.log(studentName);
+console.log(studentAge);
+console.log(studentCourse);
+
+
+// Task 2: Var, Let, Const
+var company = "Stackly";
+let salary = 20000;
+const country = "India";
+company = "Google";
+salary = 30000
+
+console.log(company);
+console.log(salary);
+
+// Task 3: User Name
+
+var name = prompt("Enter Name : ")
+console.log("Welcome " , name)
+
+
+// Task 4: Confirm Box
+
+console.log(confirm("Do you know JavaScript?"))
+
+
+// Task 5: Student Information
+let studentName = input("Enter name: ")
+let studentAge = input("Enter age: ")
+let studentCity = input("Enter city: ")
+
+console.log("Name : " , studentName)
+console.log("Age :  " , studentAge)
+console.log("City : " , studentCity)
+
+
+// Task 6: Alert Message
+let course = "JavaScript";
+alert("Welcome to", course)
+
+// Task 7: Document Write
+document.writeln("Hello Student Welcome to JavaScript")
+
+// Display the following in the browser using document.writeln():
+// Hello Student
+// Welcome to JavaScript
+
+
+// Task 8: Favorite Things
+var favoriteFood="Chicken Biryani"
+var favoriteColor="Black"
+var favoriteMovie=" Leo"
+
+console.log("Food:",favoriteFood)
+console.log("color:",favoriteColor)
+console.log("Movie:",favoriteMovie)
+
+
+// Task 9: Mini Bio Data
+var name="Rishi"
+const age=26
+const phone=9600377981
+let email="rishi@gmail.com"
+
+console.log("name");
+console.log("age");
+console.log("phone");
+console.log("email")
+
+
+// Task 10 (Challenge)""
+var Name=input("Enter a Name")
+var Course=input("Enter a Course")
+alert("Hello ", Name , " , " , "Welcome to ",Course," Course")
